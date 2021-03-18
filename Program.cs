54fbaf2efa3d9a4ed202b9dd32635c1bf54ef92f
João Alves <joao.alves@liberali.local>
2021-03-18 08:18:40 -0400

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
