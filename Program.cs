49dbbfb78981e63157635f40beadcd92a4012129
João Alves <joao.alves@liberali.local>
2021-03-18 09:33:13 -0400

    Criação das classes de peças do tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace ChessProject
 
@@ -9,6 +10,14 @@ namespace ChessProject
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8,8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
+
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
         }
     }
 }
--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -4,17 +4,24 @@ namespace tabuleiro
 {
     class Peca
     {
+
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int QteMovimentos { get; protected set; }
         public Tabuleiro tab { get; set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Cor cor, Tabuleiro tab)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.cor = cor;
             this.tab = tab;
             this.QteMovimentos = 0;
+        }
+
+        public Peca(Tabuleiro tab, Cor cor)
+        {
+            this.tab = tab;
+            this.cor = cor;
         }
     }
 }
