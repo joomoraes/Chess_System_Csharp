dfa1129d464fa7836885f63c87f4c691d7f84233
João Alves <joao.alves@liberali.local>
2021-03-18 09:57:14 -0400

    Criação da classe PosicaoXadrez

--- Program.cs
+++ Program.cs
@@ -9,19 +9,12 @@ namespace ChessProject
     {
         static void Main(string[] args)
         {
-            try { 
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-            Tabuleiro tab = new Tabuleiro(8,8);
+            Console.WriteLine(pos);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
+            Console.WriteLine(pos.toPosicao());
 
-            Tela.imprimirTabuleiro(tab);
-            } catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
             Console.ReadLine();
         }
     }
