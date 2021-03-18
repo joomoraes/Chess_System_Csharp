20c5fa27f380a7ee14a94549cb4859f377421ea6
João Alves <joao.alves@liberali.local>
2021-03-18 10:24:54 -0400

    Melhoria de tela

--- Program.cs
+++ Program.cs
@@ -9,11 +9,23 @@ namespace ChessProject
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            Console.WriteLine(pos.toPosicao());
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(3, 5));
+
+                Tela.imprimirTabuleiro(tab);
+            } catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+           
 
             Console.ReadLine();
         }
