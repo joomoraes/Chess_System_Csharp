e8a577bea0842957e801e3f3d3b29c1f62145db5
João Alves <joao.alves@liberali.local>
2021-03-18 08:47:59 -0400

    Atribuição da folder posição

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
+using tabuleiro;
 namespace ChessProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + P);
+
+            Console.ReadLine();
         }
     }
 }
